6df0900a4cbe1d0af57a6dcaab31589bf87033dc
Thiago Linchin <dev.thiagolinch@gmail.com>
2024-07-12 08:53:20 -0400

    axios_update

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://vercel.com/devthiagolinchs-projects/nlw-journey-nodejs/HyTqgwFYiyrzKfwzWWjKsKn5MfGx'
 })
